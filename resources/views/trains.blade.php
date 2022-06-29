309b58794e6d2d3018b7459d443c919b039bb15d
alexcapoluongo <alexcapoluongo@icloud.com>
2022-06-29 16:55:36 +0200

    list trains with details added

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -1,4 +1,13 @@
 <h1>lista treni</h1>
 @foreach ($trains as $item)   
-<p>{{ $item->azienda }}</p>
+<h2>Compagnia: {{ $item->azienda }}</h2>
+<p>Città di partenza: {{ $item->stazione_partenza }}</p>
+<p>Città di arrivo: {{ $item->stazione_arrivo }}</p>
+<p>Orario Partenza: {{ $item->orario_partenza }}</p>
+<p>Orario Arrivo: {{ $item->orario_arrivo }}</p>
+<p>Codice treno: {{ $item->codice_treno }}</p>
+<p>Numero carrozza: {{ $item->numero_carrozza }}</p>
+<p>In orario: {{ $item->in_orario }}</p>
+<p>Cancellato: {{ $item->cancellato }}</p>
+
 @endforeach
