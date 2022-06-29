65ec5a2c3a758e6996d8ff31787a079af4538068
alexcapoluongo <alexcapoluongo@icloud.com>
2022-06-29 16:22:13 +0200

    added for cycle in seed

--- database/migrations/2022_06_29_125640_create_trains_table.php
+++ database/migrations/2022_06_29_125640_create_trains_table.php
@@ -15,7 +15,7 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string('azienda', 20);
+            $table->string('azienda', 50);
             $table->string('stazione_partenza', 20);
             $table->string('stazione_arrivo', 20);
             $table->time('orario_partenza');
--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -13,21 +13,22 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
-    {
-        $train = new Train();
-        $train->azienda = $faker->name();
-        $train->stazione_partenza = 'Milano Centrale';
-        $train->stazione_arrivo = 'Firenza SMN';
-        $train->orario_partenza = '15:00';
-        $train->orario_arrivo = '19:00';
-        $train->codice_treno = 'XW3RL';
-        $train->numero_carrozza = 5;
-        $train->in_orario = true;
-        $train->cancellato = false;
-
-        //salva dati nel db
-        $train->save();
-        
+    
+    public function run(Faker $faker) {
+            for ($i=0; $i < 10; $i++) { 
+                $train = new Train();
+                $train->azienda = $faker->name();
+                $train->stazione_partenza = $faker->city();
+                $train->stazione_arrivo = $faker->city();
+                $train->orario_partenza = $faker->numberBetween( 1, 24 );
+                $train->orario_arrivo = $faker->numberBetween( 1, 24 );
+                $train->codice_treno = 'XW3RL';
+                $train->numero_carrozza = 5;
+                $train->in_orario = true;
+                $train->cancellato = false;
+    
+                //salva dati nel db
+                $train->save();  
+        }
     }
 }
